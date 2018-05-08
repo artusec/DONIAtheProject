2ca8eafa0b3c333729114606030ec3882d40292f
Raúl <Ral@LAPTOP-3N5H8IBS>
2018-05-08 15:45:32 +0200

    Cancion y lo que hemos podido de genero

--- src/Model/Genero/SASGenero.java
+++ src/Model/Genero/SASGenero.java
@@ -1,7 +1,9 @@
 package Model.Genero;
 
 import Controlador.DAO.InterfazDAOFachada;
+import Excepciones.ErrorConsulta;
 import Model.Objetos.Genero;
+import Model.Objetos.Usuario;
 
 public class SASGenero implements InterfazSASGenero {
 	
@@ -12,24 +14,29 @@ public class SASGenero implements InterfazSASGenero {
 	 * Anade un genero a la DB
 	 * @param genero genero a anadir
 	 */
-    public void Anadir(Genero genero) {
-    		dao.setGenero(genero);
+    public void Anadir(Usuario usuario, Genero genero) { //Falta acabar setgenero
+    		dao.setGenero(usuario, genero);
     }
 
     /**
 	 * Elimina un genero de la DB
 	 * @param genero genero a eliminar
 	 */
-    public void Eliminar(Genero genero) {
-    		dao.borrarGeneroDB(genero);
+    public void Eliminar(Usuario usuario, Genero genero) { //Falta acabar borrargeneroDB
+    		dao.borrarGeneroDB(usuario, genero);
     	}
 
     /**
      * Obitiene un genero de la DB
      * @param id id del genero
      * @return el genero buscado, null si no existe
+     * @throws ErrorConsulta 
      */
-    public Genero Consultar(String idGenero) {
-    		return dao.getGeneroDB(idGenero);
+    public Genero Consultar(String idGenero) throws ErrorConsulta {
+    	Genero genero = dao.getGeneroDB(idGenero);
+    	if (genero == null)
+    		throw new ErrorConsulta("Error al consultar género");
+    	else
+    		return genero;
     }
 }
