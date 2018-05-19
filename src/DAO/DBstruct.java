65d5dde709c5cb31592d24545b724198f257c20c
borjawer <borjamonpatanegra@hotmail.com>
2018-05-19 13:44:03 +0200

    javadoc dao a medias

--- src/DAO/DBstruct.java
+++ src/DAO/DBstruct.java
@@ -1,10 +1,8 @@
 package DAO;
 
 /**
- * Esta clase proporciona las serntencias SQL para la creacion de tablas, insercion,
+ * Clase DBstruct, proporciona las serntencias SQL para la creacion de tablas, insercion,
  * eliminacion y actualizacion en maria
- * 
- * @author borja
  */
 public abstract class DBstruct {
 	
@@ -213,55 +211,127 @@ public abstract class DBstruct {
 			") ENGINE=InnoDB DEFAULT CHARSET=latin1 \n";
 	
 	// IDs ENTIDADES UNICAS OBLIGATORIAS
+	/**
+	 * Devuelve la id de la biblioteca.
+	 * @return Id de la biblioteca.
+	 */
 	public static String getIdBiblioteca() {
 		return idBiblioteca;
 	}
+	/**
+	 * Devuelve la id del administrador.
+	 * @return Id del administrador.
+	 */
 	public static String getIdUsuarioAdmin() {
 		return idUsuarioAdmin;
 	}
 	
 	// SENTENCIAS CREACION TABLAS
+	/**
+	 * Devuelve todas las sentencias para la creacion de todas las tablas.
+	 * @return La sentencia.
+	 */
 	public static String getInitTodo() {
 		return initTodo;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla cancion.
+	 * @return La sentencia.
+	 */
 	public static String getCancion() {
 		return cancion;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla video.
+	 * @return La sentencia.
+	 */
 	public static String getVideo() {
 		return video;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla letra.
+	 * @return La sentencia.
+	 */
 	public static String getLetra() {
 		return letra;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla genero.
+	 * @return La sentencia.
+	 */
 	public static String getGenero() {
 		return genero;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla lista.
+	 * @return La sentencia.
+	 */
 	public static String getLista() {
 		return lista;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla rlistacancion.
+	 * @return La sentencia.
+	 */
 	public static String getRlistacancion() {
 		return rlistacancion;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla listaauto.
+	 * @return La sentencia.
+	 */
 	public static String getListaauto() {
 		return listaauto;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla listanormal.
+	 * @return La sentencia.
+	 */
 	public static String getListanormal() {
 		return listanormal;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla biblioteca.
+	 * @return La sentencia.
+	 */
 	public static String getBiblioteca() {
 		return biblioteca;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla usuario.
+	 * @return La sentencia.
+	 */
 	public static String getUsuario() {
 		return usuario;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla rusuariogenero.
+	 * @return La sentencia.
+	 */
 	public static String getRusuariogenero() {
 		return rusuariogenero;
 	}
+	/**
+	 * Devuelve la sentencia para la creacion de la tabla usuarioadmin.
+	 * @return La sentencia.
+	 */
 	public static String getUsuarioadmin() {
 		return usuarioadmin;
 	}
 	
 	//SENTENCIAS DE INSERCION
+	/**
+	 * Genera una sentencia para isertar datos en la tabla cancion.
+	 * @param id Id de la canción.
+	 * @param titulo Título de la canción. 
+	 * @param autor Autor de la canción.
+	 * @param duracion Duración de la canción.
+	 * @param album Álbum de la canción.
+	 * @param genero Id del género de la canción.
+	 * @param video Id del vídeo de la canción.
+	 * @param letra Id de la letra de la canción.
+	 * @return La sentencia.
+	 */
 	public static String insertCancion(String id, String titulo, String autor, double duracion, String album,
 			String genero, String video, String letra) {
 		return 	"INSERT INTO donia.cancion\n" +
@@ -269,55 +339,101 @@ public abstract class DBstruct {
 				"VALUES('" + id + "', " + "'" + titulo + "', " + "'" + autor + "', " + "'" + duracion + "', " +
 				"'" + album + "', " + "'" + genero + "', " + "'" + video + "', " + "'" + letra + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla cancion.
+	 * @param id Id de la letra.
+	 * @param texto La letra.
+	 * @return La sentencia.
+	 */
 	public static String insertaLetra(String id, String texto) {
 		return "INSERT INTO donia.letra\n" + 
 				"(texto, letra)\n" + 
 				"VALUES('"+ texto +"', '"+ id +"');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla cancion.
+	 * @param id Id del video.
+	 * @param enlace Enlace al video.
+	 * @param descarga Enlace a la descarga del video.
+	 * @return La sentencia.
+	 */
 	public static String insertaVideo(String id, String enlace, String descarga) {
 		return "INSERT INTO donia.video\n" + 
 				"(enlace, enlaceDescarga, video)\n" + 
 				"VALUES('"+ enlace +"', '"+ descarga +"', '"+ id +"');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla usuario.
+	 * @param id Id del usuario.
+	 * @param nombre Nombre del usuario.
+	 * @param clave Clave del usuario.
+	 * @return La sentencia.
+	 */
 	public static String insertUsuario(String id, String nombre, String clave) {
 		return "INSERT INTO donia.usuario\n" +
 				"(usuario, nombre, clave)\n" +
 				"VALUES('" + id + "', '" + nombre + "', '"  + clave + "')\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla genero.
+	 * @param id Id del género.
+	 * @return La sentencia.
+	 */
 	public static String insertGenero(String id) {
 		return "INSERT INTO donia.genero\n" +
 				"(genero)\n" +
 				"VALUES('" + id + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla lista.
+	 * @param id Id de la lista.
+	 * @param nombre Nombre de la lista.
+	 * @return La sentencia.
+	 */
 	public static String insertLista(String id, String nombre) {
 		return "INSERT INTO donia.lista\n" +
 				"(lista, nombre)\n" +
 				"VALUES('" + id + "', " + "'" + nombre + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla listanormal.
+	 * @param lista Id de la lista.
+	 * @param usuario Id del usuario.
+	 * @return La sentencia.
+	 */
 	public static String insertListaNormal(String lista, String usuario) {
 		return "INSERT INTO donia.listanormal\n" +
 		"(lista, usuario)\n" +
 		"VALUES('" + lista + "', " + "'" + usuario + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla listaauto.
+	 * @param lista Id de la lista.
+	 * @param genero Id del genero.
+	 * @return La sentencia.
+	 */
 	public static String insertListaAuto(String lista, String genero) {
 		return "INSERT INTO donia.listaauto\n" +
 				"(lista, genero)\n" +
 				"VALUES('" + lista + "', " + "'" + genero + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla rlistacancion.
+	 * @param lista Id de la lista.
+	 * @param cancion Id de la canción.
+	 * @return La sentencia.
+	 */
 	public static String insertRlistaCancion(String lista, String cancion) {
 		return 	"INSERT INTO donia.rlistacancion\n" +
 				"(lista, cancion) \n" +
 				"VALUES('" + lista + "', " + "'" + cancion + "');\n";
 	}
-	
+	/**
+	 * Genera una sentencia para isertar datos en la tabla rgenerousuario.
+	 * @param genero Id del genero.
+	 * @param usuario Id del usuario.
+	 * @return La sentencia.
+	 */
 	public static String insertRgeneroUsuario(String genero, String usuario) {
 		return 	"INSERT INTO donia.rusuariogenero\n" +
 				"(genero, usuario)\n" +
@@ -325,6 +441,18 @@ public abstract class DBstruct {
 	}
 	
 	//SENTENCIAS DE ACTUALIZACION
+	/**
+	 * Genera una sentencia para actualizar datos en la tabla cancion.
+	 * @param id Id de la canción.
+	 * @param titulo Título de la canción. 
+	 * @param autor Autor de la canción.
+	 * @param duracion Duración de la canción.
+	 * @param album Álbum de la canción.
+	 * @param genero Id del género de la canción.
+	 * @param video Id del vídeo de la canción.
+	 * @param letra Id de la letra de la canción.
+	 * @return La sentencia.
+	 */
 	public static String updateCancion(String id, String titulo, String autor, double duracion, String album,
 			String genero, String video, String letra) {
 		return 	"UPDATE donia.cancion \n" +
@@ -332,23 +460,46 @@ public abstract class DBstruct {
 				", album='" + album + "', genero='" + genero + "', video='" + video + "', letra='" + letra + "' \n" +
 				"WHERE cancion='" + id + "'";
 	}
+	/**
+	 * Genera una sentencia para actualizar datos en la tabla usuario.
+	 * @param id Id del usuario.
+	 * @param nombre Nombre del usuario.
+	 * @param clave Clave del usuario.
+	 * @return La sentencia.
+	 */
 	public static String updateUsuario(String id, String nombre, String clave) {
 		return 	"UPDATE donia.usuario\n" +
 				"SET nombre='" + nombre + "', clave='" + clave + "'\n" +
 				"WHERE usuario='" + id + "';\n";
 	}
-	
+	/**
+	 * Genera una sentencia para actualizar datos en la tabla genero.
+	 * @param id Id del género.
+	 * @param nombre Id del género.
+	 * @return La sentencia.
+	 */
 	public static String updateGenero(String id, String nombre) {
 		return 	"UPDATE donia.genero\n" +
 				"SET nombre='" + nombre + "' \n" +
 				"WHERE genero='" + id + "'";
 	}
-	
+	/**
+	 * Genera una sentencia para actualizar datos en la tabla lista.
+	 * @param id Id de la lista.
+	 * @param nombre Nombre de la lista.
+	 * @return La sentencia.
+	 */
 	public static String updateLista(String id, String nombre) {
 		return "UPDATE donia.lista \n" +
 				"SET nombre='" + nombre + "' \n" +
 				"WHERE lista='" + id + "'";
 	}
+	/**
+	 * Genera una sentencia para actualizar datos en la tabla listaauto.
+	 * @param idLista Id de la lista.
+	 * @param idGenero Id del género.
+	 * @return La sentencia.
+	 */
 	public static String updateGeneroLista(String idLista, String idGenero) {
 		return "UPDATE donia.listaauto\n" + 
 				"SET genero='"+ idGenero +"' " + 
@@ -356,36 +507,66 @@ public abstract class DBstruct {
 	}
 	
 	// SENTENCIAS DE ELIMINACION
+	/**
+	 * Genera una sentencia para eliminar datos de la tabla lista.
+	 * @param idLista Id de la lista.
+	 * @return La sentencia.
+	 */
 	public static String deleteLista(String idLista) {
 		return "DELETE FROM donia.lista \n" + 
 				"WHERE lista='" + idLista + "'";
 	}
-	
+	/**
+	 * Genera una sentencia para eliminar datos de la tabla cancion.
+	 * @param idCancion Id de la canción.
+	 * @return La sentencia.
+	 */
 	public static String deleteCancion(String idCancion) {
 		return "DELETE FROM donia.cancion \n" + 
 				"WHERE cancion='" + idCancion + "'";
 	}
-	
+	/**
+	 * Genera una sentencia para eliminar datos de la tabla genero.
+	 * @param idGenero Id del género.
+	 * @return La sentencia.
+	 */
 	public static String deleteGenero(String idGenero) {
 		return "DELETE FROM donia.genero \n" + 
 				"WHERE genero='" + idGenero + "'";
 	}
+	/**
+	 * Genera una sentencia para eliminar datos de la tabla usuario.
+	 * @param idUsuario Id del usuario.
+	 * @return La sentencia.
+	 */
 	public static String deleteUsuario(String idUsuario) {
 		return "DELETE FROM donia.usuario \n" + 
 				"WHERE usuario='" + idUsuario + "'";
 	}
-	
+	/**
+	 * Genera una sentencia para eliminar las canciones de una lista (en la tabla rlistacancion).
+	 * @param idLista Id de la lista.
+	 * @return La sentencia.
+	 */
 	public static String deleteRlistaCancion(String idLista) {
 		return "DELETE FROM donia.rlistacancion \n" + 
 				"WHERE lista='" + idLista + "'";
 	}
-	
+	/**
+	 * Genera una sentencia para eliminar datos de la tabla rgenerousuario.
+	 * @param idUsuario Id del usuario.
+	 * @param idGenero Id del género.
+	 * @return La sentencia.
+	 */
 	public static String deleteRgeneroUsuario(String idUsuario, String idGenero) {
 		return "DELETE FROM donia.rusuariogenero\n" + 
 				"WHERE usuario='" + idUsuario + "' AND genero='" + idGenero + "';";
 	}
-	
-	
+	/**
+	 * Genera una sentencia para eliminar multiples datos de la tabla rgenerousuario.
+	 * @param idUsuario Id del usuario.
+	 * @return La sentencia.
+	 */
 	public static String deleteRgenerosUsuario(String idUsuario) {
 		return "DELETE FROM donia.rusuariogenero \n" + 
 				"WHERE usuario='" + idUsuario + "'";
--- src/DAO/InterfazDAOFachada.java
+++ src/DAO/InterfazDAOFachada.java
@@ -9,13 +9,63 @@ import Excepciones.ErrorEliminacion;
 import Excepciones.ErrorGuardado;
 import Model.Objetos.*;
 
+/**
+ * Clase InterfazDAOFachada, contiene las funciones sin implementar que conforman la
+ * fachada del DAO.
+ */
 public interface InterfazDAOFachada {
 	// GET
+	/**
+	 * Solicita la información de un género a la base de datos.
+	 * @param idGenero Id del género.
+	 * @return El género, null si no se ha podido encontrar.
+	 * @throws ErrorConsulta Si se ha producido un error al buscar el género.
+	 * @throws ErrorCreacionObjeto Si se ha producido un error al crear el objeto.
+	 */
 	public Genero getGeneroDB(String idGenero) throws ErrorConsulta, ErrorCreacionObjeto;
+	/**
+	 * Solicita la información de una canción a la base de datos.
+	 * @param idCancion
+	 * @return
+	 * @throws ErrorCreacionObjeto
+	 * @throws ErrorConsulta
+	 */
 	public Cancion getCancionDB(String idCancion) throws ErrorCreacionObjeto, ErrorConsulta;
+	/**
+	 * Solicita la lista de canciones de un género a la base de datos.
+	 * @param id
+	 * @return
+	 * @throws ErrorConsulta
+	 * @throws ErrorCreacionObjeto
+	 */
 	public ArrayList<Cancion> getCancionesGeneroDB(String id) throws ErrorConsulta, ErrorCreacionObjeto;
+	/**
+	 * Solicita la información de una lista a la base de datos.
+	 * @param idLista
+	 * @return
+	 * @throws ErrorConsulta
+	 * @throws ErrorCreacionObjeto
+	 */
 	public Lista getListaDB(String idLista) throws ErrorConsulta, ErrorCreacionObjeto;
+	/**
+	 * Solicita la información de un usuario a la base de datos.
+	 * @param idUsuario
+	 * @param clave
+	 * @return
+	 * @throws ErrorAutenticacion
+	 * @throws ErrorConsulta
+	 * @throws ErrorCreacionObjeto
+	 */
 	public Usuario getUsuarioDB(String idUsuario, String clave) throws ErrorAutenticacion, ErrorConsulta, ErrorCreacionObjeto;
+	/**
+	 * Solicita una lista que contenga todas las listas de un usuario a la base de datos.
+	 * @param idUsuario
+	 * @param clave
+	 * @return
+	 * @throws ErrorAutenticacion
+	 * @throws ErrorConsulta
+	 * @throws ErrorCreacionObjeto
+	 */
 	public ArrayList<Lista> getListasDB(String idUsuario, String clave) throws ErrorAutenticacion, ErrorConsulta, ErrorCreacionObjeto;
 	// SET
 	public void setCancion(Cancion cancion) throws ErrorGuardado, ErrorCreacionObjeto;
