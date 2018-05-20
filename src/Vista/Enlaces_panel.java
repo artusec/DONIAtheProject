234a62f002566498076883e46aad85c9961bce94
borjawer <borjamonpatanegra@hotmail.com>
2018-05-20 12:24:22 +0200

    ARTURO TE LO PROMETI para compensarte por lo de r2

--- src/Vista/Enlaces_panel.java
+++ src/Vista/Enlaces_panel.java
@@ -1,21 +1,21 @@
 package Vista;
 
-import javax.swing.JPanel;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JLabel;
+import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 
 import Controlador.ControlCancion;
 import Model.Objetos.Cancion;
 import Model.Objetos.Video;
-
-import javax.swing.JButton;
 import net.miginfocom.swing.MigLayout;
 
 public class Enlaces_panel extends JPanel {
@@ -31,25 +31,37 @@ public class Enlaces_panel extends JPanel {
 	 */
 
 	public Enlaces_panel(VentanaPrincipal ventanaPrincipal) {
-		setLayout(new MigLayout("", "[][405.00,center]", "[20px][][22px][20px][22px][25px][][][][][][]"));
-
-
+		setLayout(new MigLayout("", "[363.00,center][55.00,grow,left]", "[20px][][22px][20px][22px][25px][][][][][][]"));
 		
 		JLabel lblEnlaceVerVideo = new JLabel("Ver video");
 		lblEnlaceVerVideo.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 16));
-		add(lblEnlaceVerVideo, "cell 1 1,alignx center,aligny top");
+		add(lblEnlaceVerVideo, "cell 0 1,alignx center,aligny top");
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-		add(textField, "cell 1 2,growx,aligny top");
+		add(textField, "cell 0 2,growx,aligny top");
+		
+		JButton botonVer = new JButton("Ver");
+		botonVer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Desktop.isDesktopSupported()) {
+				    try {
+						Desktop.getDesktop().browse(new URI(textField.getText()));
+					} catch (IOException | URISyntaxException e1) {
+						VentanaPrincipal.muestraError(new Exception("Dirección incorrecta!"));
+					}
+				}
+			}
+		});
+		add(botonVer, "cell 1 2");
 		
 		JLabel lblNewLabel = new JLabel("Enlace descarga");
 		lblNewLabel.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 16));
-		add(lblNewLabel, "cell 1 4,alignx center,aligny top");
+		add(lblNewLabel, "cell 0 4,alignx center,aligny top");
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		add(textField_1, "cell 1 5,growx,aligny top");
+		add(textField_1, "cell 0 5,growx,aligny top");
 		
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.addActionListener(new ActionListener() {
@@ -59,7 +71,21 @@ public class Enlaces_panel extends JPanel {
 				ventanaPrincipal.verPanelLetras();
 			}
 		});
-		add(btnVolver, "cell 1 9,alignx center,aligny top");
+		
+		JButton botonDesc = new JButton("Descargar");
+		botonDesc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (Desktop.isDesktopSupported()) {
+				    try {
+						Desktop.getDesktop().browse(new URI(textField_1.getText()));
+					} catch (IOException | URISyntaxException e1) {
+						VentanaPrincipal.muestraError(new Exception("Dirección incorrecta!"));
+					}
+				}
+			}
+		});
+		add(botonDesc, "cell 1 5");
+		add(btnVolver, "cell 0 9,alignx center,aligny top");
 		
 		setVisible(true);
 
